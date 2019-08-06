4503b56c14f47dc091dffde815a6a241e8b3c480
ireneannx <irene2104@gmail.com>
2019-08-06 19:21:01 +0530

    ..

--- express-intro/app.js
+++ express-intro/app.js
@@ -12,8 +12,9 @@ app.get("/registration",(req,res)=>{
 })
 
 //link to login.html in static folder
-app.use(express.static("static")); //express.static will by default take /. Priority is given to whicherver line comes first. Since / is already defined in line 6, it will not read static file
+app.use(express.static("static")); //express.static will by default take /. Priority is given to whicherver line comes first. Since / is already defined in line 6, it will not read static file. Thaz why i've commented it 
 
-//app.use("/login",express.static("static"))
+//keep an index file in each folder (such as for login folder. browser will automatically go to index.html in that folder)
+// url to get to login page : http://localhost:1234/login/
 
 app.listen(port,()=>{console.log(`server started at ${port}`)});
